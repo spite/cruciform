51e27a87f774404a542b6810b14de1b1447c6cd4
Jaume Sanchez <the.spite@gmail.com>
2015-02-09 16:38:09 +0100

    upload

---
+++ js/storyboard.js
@@ -0,0 +1,152 @@
+var storyline = parseStoryline( {
+  
+  	x: [
+  		'0 cut to -5',
+  		'22 cut to 5',
+  		'51.4 linear to -2',
+  		'51.4 cut to 11',
+  		'64.5 linear to 1',
+  		'64.5 cut to -9.3',
+  		'80 cut to -1',
+  		'96 linear to -5.3',
+  		'96 cut to -1.5',
+  		'120 linear to 12'
+  	],
+
+    y: [
+		'0 cut to -1',
+		'22 linear to 1',
+		'22 cut to -2.2',
+		'51.4 cut to -3',
+		'64.5 linear to 0',
+		'64.5 cut to -3.1',
+		'80 linear to 1.3',
+		'80 cut to 5',
+		'96 cut to 0',
+		'120 linear to 3'
+	],
+
+	z: [
+		'0 cut to 0',
+		'22 cut to -0.788193584006919',
+		'51.4 cut to 0',
+		'64.5 cut to 0.04',
+		'80 cut to 0',
+		'96 cut to 0'
+	],
+
+	targetx: [
+		'0 cut to 1',
+ 		'22 cut to -1.5',
+  		'51.4 linear to -5,5',
+  		'51.4 cut to -11',
+  		'64.5 cut to 0',
+  		'80 cut to -1',
+  		'96 linear to -5.3',
+  		'96 cut to -3'
+ 	],
+
+	targety: [
+		'0 cut to -1',
+		'22 linear to 1',
+		'22 cut to -3.3645479983609117',
+		'51.4 cut to 4',
+		'64.5 cut to -3.1',
+		'80 linear to 1.3',
+		'80 cut to 0',
+		'96 cut to 0'
+	],
+
+	targetz: [
+		'0 cut to 0',
+		'22 cut to -3.0881100174072027',
+		'51.4 cut to 0',
+		'64.5 cut to 0',
+		'96 cut to 0',
+		'96 cut to 0'
+	],
+
+	fade: [
+		'0 cut to 0',
+		'3.5 linear to 1',
+		'3.5 cut to 1',
+		'110 cut to 1',
+		'115 linear to 0'
+	],
+
+	text: [
+		'0 cut to 0',
+		'7 linear to 0',
+		'9 linear to 1',
+		'12 linear to 1',
+		'15 linear to 0',
+		'114 cut to 0',
+		'116 linear to 1'
+	],
+
+	credits: [
+		'118 cut to 0',
+		'120 linear to 1'
+	],
+
+	sphere: [
+		'0 cut to 0',
+		'59 cut to 0',
+		'64.5 ease to 1'
+	],
+
+	sphereAngle: [
+		'0 cut to 0',
+		'59 cut to 0',
+		'120 linear to 6.28'
+	],
+
+	fov: [ 
+		'0 cut to 75',
+		'22 cut to 60',
+		'51.4 linear to 40',
+		'51.4 cut to 75',
+		'64.5 cut to 75',
+		'80 linear to 50',
+		'80 cut to 50',
+		'96 cut to 75'
+	],
+
+	orbx: [
+		'0 cut to -4',
+		'64.5 cut to 0',
+		'80 linear to -6',
+ 		'80 cut to -1',
+  		'96 linear to -5.3',
+  		'96 cut to -6',
+  		'96 linear to -4'
+	],
+
+	autofocus: [
+		'0 cut to 0',
+		'22 cut to 0',
+		'51.4 cut to 0',
+		'64.5 cut to 0',
+		'80 cut to 1',
+		'96 cut to 1',
+	],
+
+	focalDistance: [
+		'22 cut to .83',
+		'64.5 cut to .7',
+		'80 linear to .9'
+	],
+
+	aperture: [
+		'0 cut to 0',
+		'22 cut to .01',
+		'51.4 linear to .02',
+		'51.4 cut to 0',
+		'64.5 cut to .001',
+		'80 linear to 0',
+		'80 cut to .01',
+		'96 cut to .02',
+		'100 linear to 0'
+	]
+
+} );
